d4a48fe4ba503367ff610eaf3b14cbb94236187f
Navulon <danialfilatov@gmail.com>
2025-05-23 23:23:42 +0200

    1. Added class ResourceDeposit 2. Added class ResourceExtractor

---
+++ BusinessShark/Core/ResourceDeposit.cs
@@ -0,0 +1,22 @@
+﻿using BusinessShark.Core.Item;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessShark.Core
+{
+    internal class ResourceDeposit(
+        ItemDefinition definition,
+        Point location
+        ) 
+    {
+        // this will have to be redone in the future
+        public ItemDefinition ResourceDefinition = definition;
+        public int ResourceQuality = 0;
+        public int ResourceRichness = 0;
+        string name;
+    }
+}
---
+++ BusinessShark/Core/ResourceExtractor.cs
@@ -0,0 +1,74 @@
+﻿using BusinessShark.Core.Item;
+
+using System.Drawing;
+
+
+namespace BusinessShark.Core
+{
+    internal class ResourceExtractor(
+        int divisionId,
+        ItemDefinition resourceDefinition,
+        float techLevel,
+        Tools toolPark,
+        Workers workers,
+        Point location) : DeliveryDivision(divisionId, location)
+
+    {
+        public ItemDefinition ResourceDefinition = resourceDefinition;
+        public float ProgressProduction; //procent of single product left on production
+        public float ProgressQuality;
+        public float ResourceDepositQuality = 5; // replace with the quality of this type of resource at the deposit
+        public float TechLevel = techLevel;
+        public Tools ToolPark = toolPark;
+        public Workers FactoryWorkers = workers;
+
+        public override void StartCalculation()
+        {
+            if (ProgressProduction == 0)
+            {
+                // Calculate production quality
+                ProgressQuality = CalculateResourceQuality();
+            }
+            ProgressProduction += CalculateResourceQuantity(ResourceDefinition.BaseProductionCount);
+
+            if (ProgressProduction >= 1)
+            {
+                var productionCount = (int)Math.Round(ProgressProduction);
+
+                if (WarehouseInput.TryGetValue(ResourceDefinition.ItemDefinitionId, out Item.Item? storedItem))
+                {
+                    storedItem.ProcessingQuality = ProgressQuality;
+                    WarehouseInput[ResourceDefinition.ItemDefinitionId].ProcessingQuantity += productionCount;
+                }
+                else
+                {
+                    WarehouseInput[ResourceDefinition.ItemDefinitionId] =
+                        new Item.Item(ResourceDefinition, productionCount, ProgressQuality);
+                }
+
+                ProgressProduction -= 1;
+            }
+        }
+
+        public override void CompleteCalculation()
+        {
+            
+        }
+
+        internal float CalculateResourceQuality()
+        {
+            float TotalQuality = ResourceDepositQuality * (TechLevel * ResourceDefinition.TechImpactQuality
+                            + ToolPark.TechLevel * ResourceDefinition.ToolImpactQuality
+                            + FactoryWorkers.TechLevel * ResourceDefinition.WorkerImpactQuality);
+            return TotalQuality;
+        }
+
+        internal float CalculateResourceQuantity(float baseProductionCount)
+        {
+            float TotatlQuantity = baseProductionCount * (TechLevel * ResourceDefinition.TechImpactQuantity
+                            + ToolPark.TechLevel * ResourceDefinition.ToolImpactQuantity
+                            + FactoryWorkers.TechLevel * ResourceDefinition.WorkerImpactQuantity);
+            return TotatlQuantity;
+        }
+    }
+}
